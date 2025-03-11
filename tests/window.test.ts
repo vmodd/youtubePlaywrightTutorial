3fa03121e1a0aca45aa5cf0becb052e4d507b472
Vlad Moisa <vlad.moisa@yahoo.com>
2025-03-11 14:18:58 +0200

    Added new test for window opening

---
+++ tests/window.test.ts
@@ -0,0 +1,13 @@
+import {test, expect} from "@playwright/test"
+
+test("window opening test", async ({page}) => {
+    
+        await page.goto("https://www.lambdatest.com/selenium-playground/window-popup-modal-demo");
+
+      const [newWindow] = await Promise.all([
+        page.waitForEvent("popup"),
+        page.getByText("Follow On Twitter").click()
+         ]);
+       console.log(newWindow.url());
+
+})
