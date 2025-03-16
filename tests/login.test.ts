8cf76391a66972cb9e131c41bfe29c96c382519f
Vlad Moisa <vlad.moisa@yahoo.com>
2025-03-16 12:53:43 +0200

    commented login.test and checkbox.test temporarily

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -1,25 +1,25 @@
-import {chromium, test} from "@playwright/test"
-import { text } from "stream/consumers";
+// import {chromium, test} from "@playwright/test"
+// import { text } from "stream/consumers";
 
-test("Login demo test", async ()=>{
-    const browser= await chromium.launch(
-        {headless : false}
-    );
-    const context = await browser.newContext();
-    const page = await context.newPage();
+// test("Login demo test", async ()=>{
+//     const browser= await chromium.launch(
+//         {headless : false}
+//     );
+//     const context = await browser.newContext();
+//     const page = await context.newPage();
 
-    await page.goto("https://ecommerce-playground.lambdatest.io");
-    await page.hover("//a[@data-toggle='dropdown']//span[contains(.,'My account')]");
-    await page.click("text =Login");
+//     await page.goto("https://ecommerce-playground.lambdatest.io");
+//     await page.hover("//a[@data-toggle='dropdown']//span[contains(.,'My account')]");
+//     await page.click("text =Login");
 
-    await page.fill("input[id='input-email']", "vlad")
-    await page.fill("input[id=input-password]", "vlad")
-    await page.click("input[value = 'Login']");
+//     await page.fill("input[id='input-email']", "vlad")
+//     await page.fill("input[id=input-password]", "vlad")
+//     await page.click("input[value = 'Login']");
 
-    await page.waitForTimeout(5000)
-    const newContext= await browser.newContext()
-    const newPage = await newContext.newPage();
+//     await page.waitForTimeout(5000)
+//     const newContext= await browser.newContext()
+//     const newPage = await newContext.newPage();
 
-    await newPage.waitForTimeout(5000)
+//     await newPage.waitForTimeout(5000)
 
-}) 
+// }) 
